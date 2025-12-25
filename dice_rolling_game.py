7edb5cf0cb02cbbeeb4e00ad6ffee70080d20f00
Deoxygen24 <lemzoxy24@gmail.com>
2025-12-26 00:02:10 +0100

    _Add guessing game

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -3,8 +3,7 @@ import random
 def roll_dice():
     num1 = random.randint(1,6)
     num2 = random.randint(1,6)
-    print ("Roll the dice? (y/n): ")
-    user_response = input().lower()
+    user_response = input("Roll the dice? (y/n): ")
     if user_response == "y":
         print(f"({num1}, {num2})")
     elif user_response ==  "n":
