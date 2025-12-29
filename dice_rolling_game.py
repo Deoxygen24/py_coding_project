907fcc386da4e770b24da6e911964a31a72df977
Deoxygen24 <lemzoxy24@gmail.com>
2025-12-29 16:15:56 +0100

    Code fixes

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -1,13 +1,33 @@
+# importing the required module
 import random
 
+#this function will ask the user whether he/she want to play again or not ?.
 def roll_dice():
-    num1 = random.randint(1,6)
-    num2 = random.randint(1,6)
-    user_response = input("Roll the dice? (y/n): ")
-    if user_response == "y":
-        print(f"({num1}, {num2})")
-    elif user_response ==  "n":
-        print("Thanks for playing")
-    else:
-        print("Invalid Choice!")
-roll_dice()
+    print('\nDo you want to play again ?')
+    print('Enter Yes/No')
+    choice = input()
+
+    # if the user doesn't enter anything,he/she will be asked to enter again.
+    if choice =='':
+        print('Wrong Input !!! Enter again Yes/No')
+        choice = roll_dice()
+    return choice
+
+if __name__ == '__main__':
+    print('Welcome to the Dice Game !!!')
+    dice_value1 = random.randint(1, 6)
+    dice_value2 = random.randint(1, 6)
+    print('You got ', dice_value1 , dice_value2)
+    choice = roll_dice()
+
+    #this while loop will run untill the user doesn't want to play again.
+    while choice:
+        if choice == 'No' or choice == 'no':
+            break
+        elif choice == 'Yes' or choice == 'yes':
+            dice_value = random.randint(1, 6)
+            print('You got ', dice_value1 , dice_value2)
+            choice = roll_dice()
+        else:
+            print('Wrong Input !!! Enter again Yes/No')
+            choice = roll_dice()
